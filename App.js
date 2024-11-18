06bd03512a2b11e36d5c5a10089450ab5d355249
Leonardo Conde Feitosa <84253492+LeoKondii@users.noreply.github.com>
2024-11-18 12:50:06 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -6,6 +6,13 @@ import Login from './src/components/screens/Login'
 import Cadastro from './src/components/screens/Cadastro'
 import RecuperarSenha from './src/components/screens/RecuperarSenha'
 import AgradecimentoParticipacao from './src/components/screens/AgradecimentoParticipacao'
+import Home from './src/components/screens/Home';
+import NovaPesquisa from './src/components/screens/NovaPesquisa';
+import AcoesPesquisa from './src/components/screens/AcoesPesquisa';
+import ModificarPesquisa from './src/components/screens/ModificarPesquisa';
+import Coleta from './src/components/screens/Coleta';
+import Relatorio from './src/components/screens/Relatorio';
+import Drawer from './src/components/screens/Drawer';
 
 const Stack = createStackNavigator()
 
@@ -16,7 +23,7 @@ const App = () => {
                 <Stack.Screen 
                     name="Login" 
                     component={Login} 
-                    options={{ headerShown: false }}
+                    options={{ headerShown: false }} 
                 />
                 <Stack.Screen 
                     name="Cadastro" 
@@ -33,7 +40,7 @@ const App = () => {
                 />
                 <Stack.Screen 
                     name="RecuperarSenha" 
-                    component={RecuperarSenha}
+                    component={RecuperarSenha} 
                     options={{
                         title: 'Recuperação de Senha',
                         headerStyle: estilos.headerDefault,
@@ -43,14 +50,85 @@ const App = () => {
                 />
                 <Stack.Screen 
                     name="AgradecimentoParticipacao" 
-                    component={AgradecimentoParticipacao}
+                    component={AgradecimentoParticipacao} 
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="Home" 
+                    component={Home} 
                     options={{
-                        title: 'Obrigado!',
+                        title: 'Home',
                         headerStyle: estilos.headerDefault,
                         headerTitleStyle: estilos.headerTitle,
+                        headerLeftContainerStyle: estilos.headerLeft,
                         headerTintColor: '#372775',
+                        headerBackTitle: null,
+                        headerBackImageStyle: estilos.navigationBackImage,
                     }}
                 />
+                <Stack.Screen 
+                    name="NovaPesquisa" 
+                    component={NovaPesquisa} 
+                    options={{
+                        title: 'Nova Pesquisa',
+                        headerStyle: estilos.headerDefault,
+                        headerTitleStyle: estilos.headerTitle,
+                        headerLeftContainerStyle: estilos.headerLeft,
+                        headerTintColor: '#372775',
+                        headerBackTitle: null,
+                        headerBackImageStyle: estilos.navigationBackImage,
+                    }}
+                />
+                <Stack.Screen 
+                    name="AcoesPesquisa" 
+                    component={AcoesPesquisa} 
+                    options={{
+                        title: 'Carnaval',
+                        headerStyle: estilos.headerDefault,
+                        headerTitleStyle: estilos.headerTitle,
+                        headerLeftContainerStyle: estilos.headerLeft,
+                        headerTintColor: '#372775',
+                        headerBackTitle: null,
+                        headerBackImageStyle: estilos.navigationBackImage,
+                    }}
+                />
+                <Stack.Screen 
+                    name="ModificarPesquisa" 
+                    component={ModificarPesquisa} 
+                    options={{
+                        title: 'Modificar Pesquisa',
+                        headerStyle: estilos.headerDefault,
+                        headerTitleStyle: estilos.headerTitle,
+                        headerLeftContainerStyle: estilos.headerLeft,
+                        headerTintColor: '#372775',
+                        headerBackTitle: null,
+                        headerBackImageStyle: estilos.navigationBackImage,
+                    }}
+                />
+                <Stack.Screen 
+                    name="Coleta" 
+                    component={Coleta} 
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen 
+                    name="Relatorio" 
+                    component={Relatorio} 
+                    options={{
+                        title: 'Relatorio',
+                        headerStyle: estilos.headerDefault,
+                        headerTitleStyle: estilos.headerTitle,
+                        headerLeftContainerStyle: estilos.headerLeft,
+                        headerTintColor: '#372775',
+                        headerBackTitle: null,
+                        headerBackImageStyle: estilos.navigationBackImage,
+                    }}
+                />
+                <Stack.Screen 
+                    name="Drawer" 
+                    component={Drawer} 
+                    options={{ headerShown: false }}
+                />
+
             </Stack.Navigator>
         </NavigationContainer>
     )
